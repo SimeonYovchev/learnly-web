069434d6ae0860518892e9be43c5fa303110605b
SimeonYovchev <simeonyovchev@gmail.com>
2019-07-23 13:26:29 +0300

    customize the theme

--- src/theme/index.js
+++ src/theme/index.js
@@ -1,18 +1,12 @@
 import { createMuiTheme } from '@material-ui/core';
 
-// import palette from './palette';
-// import typography from './typography';
+import palette from './palette';
+import typography from './typography';
 // import overrides from './overrides';
 
 const theme = createMuiTheme({
-  typography: {
-    fontFamily: [
-      'Muli',
-      'Roboto',
-      'Helvetica Neue',
-      'sans-serif',
-    ],
-  },
+  palette,
+  typography,
   overrides: {
     MuiOutlinedInput: {
       root: {
---
+++ src/theme/palette.js
@@ -0,0 +1,47 @@
+export default {
+  common: {
+    neutral: '#E4E7EB',
+    muted: '#9EA0A4',
+  },
+  primary: {
+    main: '#0767DB',
+    light: '#F6F9FD',
+    dark: '#0B48A0',
+  },
+  secondary: {
+    main: '#7d58ff',
+    light: '',
+    dark: '#37248F',
+  },
+  success: {
+    main: '#45B880',
+    light: '#F1FAF5',
+    dark: '#00783E',
+  },
+  info: {
+    main: '#1070CA',
+    light: '#F1FBFC',
+    dark: '#007489',
+  },
+  warning: {
+    main: '#FFB822',
+    light: '#FDF8F3',
+    dark: '#95591E',
+  },
+  danger: {
+    main: '#ED4740',
+    light: '#FEF6F6',
+    dark: '#BF0E08',
+  },
+  text: {
+    primary: '#12161B',
+    secondary: '#66788A',
+    disabled: '#A6B1BB',
+  },
+  background: {
+    default: '#f8fafc',
+    dark: '#172B4D',
+  },
+  border: '#DFE3E8',
+  divider: '#DFE3E8',
+};

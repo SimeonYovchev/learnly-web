3ca2ef2b965443ae9e032c80d50f78a8b186dd6b
SimeonYovchev <simeonyovchev@gmail.com>
2019-07-24 17:43:25 +0300

    style WordCard component

--- src/components/WordCard/WordCard.js
+++ src/components/WordCard/WordCard.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 import {
   Grid,
   Paper,
@@ -9,6 +10,7 @@ import {
 } from '@material-ui/core';
 import { AccessTime, CheckCircleOutline } from '@material-ui/icons';
 import useWordCardStyle from './style';
+import { formatLastPracticed } from '../../utils';
 
 const WordCard = ({ word }) => {
   const classes = useWordCardStyle();
@@ -21,39 +23,41 @@ const WordCard = ({ word }) => {
       sm={6}
       xs={12}
     >
-      <Paper className={classes.root}>
-        <Typography
-          className={classes.title}
-          variant="h3"
-        >
-          {word.text}
-        </Typography>
-        <Typography className={classes.description} variant="body1">
-          {word.translations[0].text}
-        </Typography>
-        <Divider />
-        <div className={classes.stats}>
-          <AccessTime className={classes.lastPracticedIcon} />
-          <Tooltip disableFocusListener disableTouchListener title="Last practiced">
-            <Typography
-              className={classes.lastPracticedText}
-              variant="body2"
-            >
-              2hr ago
-            </Typography>
-          </Tooltip>
-          <CheckCircleOutline className={classes.totalPracticedIcon} />
-          <Tooltip disableFocusListener disableTouchListener title="Total practiced">
-            <Typography
-              className={classes.totalPracticedText}
-              variant="body2"
-            >
-              {word.answers && word.answers.length}
-              &nbsp;Times
-            </Typography>
-          </Tooltip>
-        </div>
-      </Paper>
+      <Link to={`/words/${word._id}`}>
+        <Paper className={classes.root}>
+          <Typography
+            className={classes.title}
+            variant="h3"
+          >
+            {word.text}
+          </Typography>
+          <Typography className={classes.description} variant="body1">
+            {word.translations[0].text}
+          </Typography>
+          <Divider />
+          <div className={classes.stats}>
+            <AccessTime className={classes.lastPracticedIcon} />
+            <Tooltip disableFocusListener disableTouchListener title="Last practiced">
+              <Typography
+                className={classes.lastPracticedText}
+                variant="body2"
+              >
+                {word.answers.length && formatLastPracticed(word.answers[0].createdAt)}
+              </Typography>
+            </Tooltip>
+            <CheckCircleOutline className={classes.totalPracticedIcon} />
+            <Tooltip disableFocusListener disableTouchListener title="Total practiced">
+              <Typography
+                className={classes.totalPracticedText}
+                variant="body2"
+              >
+                {word.answers && word.answers.length}
+                &nbsp;Times
+              </Typography>
+            </Tooltip>
+          </div>
+        </Paper>
+      </Link>
     </Grid>
   );
 };

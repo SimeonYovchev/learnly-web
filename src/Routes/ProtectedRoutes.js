9d69cba10dedd3db25ed733416239d249e786235
SimeonYovchev <simeonyovchev@gmail.com>
2019-07-25 11:46:12 +0300

    create word details route

--- src/Routes/ProtectedRoutes.js
+++ src/Routes/ProtectedRoutes.js
@@ -4,11 +4,13 @@ import ProtectedRoute from './ProtectedRoute';
 import Dashboard from '../components/Dashboard/Dashboard';
 import WordsList from '../components/WordsList';
 import Settings from '../components/Settings';
+import WordDetails from '../components/WordDetails/WordDetails';
 
 
 const ProtectedRoutes = () => (
   <Switch>
     <ProtectedRoute path="/dashboard" component={Dashboard} />
+    <ProtectedRoute path="/words/:id" component={WordDetails} />
     <ProtectedRoute path="/words" component={WordsList} />
     <ProtectedRoute path="/settings" component={Settings} />
     <Redirect from="/" to="/dashboard" exact component={Dashboard} />

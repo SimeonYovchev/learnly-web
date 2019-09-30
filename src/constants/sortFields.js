65d53432e7af3d0da7cbd68b41f8c71bfe27d53c
SimeonYovchev <simeonyovchev@gmail.com>
2019-09-30 12:50:39 +0300

    create Sort component

---
+++ src/constants/sortFields.js
@@ -0,0 +1,16 @@
+// eslint-disable-next-line import/prefer-default-export
+export const wordsSortFields = [
+  {
+    key: 1,
+    label: 'Date',
+    path: 'createdAt',
+  }, {
+    key: 2,
+    label: 'Last Practiced',
+    path: 'updatedAt',
+  }, {
+    key: 3,
+    label: 'Total Practiced',
+    path: 'answers.totalCount',
+  },
+];

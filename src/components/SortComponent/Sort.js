cb4bc80346fd90bfafceb67dc7ea73b09f0a80a2
SimeonYovchev <simeonyovchev@gmail.com>
2019-10-18 13:42:09 +0300

    change SortComponent in order to use SelectField component internally

--- src/components/SortComponent/Sort.js
+++ src/components/SortComponent/Sort.js
@@ -1,40 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import {
-  FormControl,
-  InputLabel,
-  Select,
-  MenuItem,
-  OutlinedInput,
-} from '@material-ui/core';
-import useSortStyles from './sortStyle';
+import SelectField from '../common/SelectField/SelectField';
 
 const Sort = ({ path, sortFields, onSort }) => {
-  const classes = useSortStyles();
 
   return (
-    <form>
-      <FormControl variant="outlined" className={classes.formControl}>
-        <InputLabel> Sort by </InputLabel>
-        <Select
-          value={path}
-          onChange={onSort}
-          labelWidth={50}
-          input={
-            <OutlinedInput name="sortBy" className={classes.input} />
-          }
-        >
-          {sortFields.map(field => (
-            <MenuItem
-              key={field.key}
-              value={field.path}
-            >
-              {field.label}
-            </MenuItem>
-          ))}
-        </Select>
-      </FormControl>
-    </form>
+    <SelectField
+      value={path}
+      label="Sort by"
+      name="sortBy"
+      options={sortFields}
+      onChange={onSort}
+    />
   );
 };
 

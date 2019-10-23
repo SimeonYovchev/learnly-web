84db3f2c5b6d4d28b2bb8661c0157fcbf36a6975
SimeonYovchev <simeonyovchev@gmail.com>
2019-10-23 11:29:59 +0300

    increase speechService rate value

--- src/services/speechService.js
+++ src/services/speechService.js
@@ -3,7 +3,7 @@ let previousWord;
 const speak = (text) => {
   const utterance = new SpeechSynthesisUtterance(text);
   utterance.pitch = 1;
-  utterance.rate = previousWord === text ? 0.5 : 1;
+  utterance.rate = previousWord === text ? 0.7 : 1;
   utterance.lang = 'en-GB';
 
   speechSynthesis.speak(utterance);
